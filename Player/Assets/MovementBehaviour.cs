641d926c26320f91d5e148d6e4aa4e7225f76546
ddlindeque <dawie.lindeque@gmail.com>
2018-03-04 14:46:46 +0000

    First version of player game object

---
+++ Player/Assets/MovementBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehaviour : MonoBehaviour {
+
+    public float walkingSpeed;
+    public float turingSpeed;
+    public float runningSpeed;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                var changeInPosition = transform.forward * Time.deltaTime * runningSpeed;
+
+                transform.position = transform.position + changeInPosition;
+            }
+            else
+            {
+                var changeInPosition = transform.forward * Time.deltaTime * walkingSpeed;
+
+                transform.position = transform.position + changeInPosition;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            var changeInPosition = -transform.forward * Time.deltaTime * walkingSpeed;
+
+            transform.position = transform.position + changeInPosition;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            var angle = Time.deltaTime * turingSpeed;
+            transform.Rotate(0f, angle, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            var angle = -Time.deltaTime * turingSpeed;
+            transform.Rotate(0f, angle, 0f);
+        }
+    }
+}
